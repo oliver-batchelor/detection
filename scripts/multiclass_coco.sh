0060c2e611df8223512e9dbf20e0a790df0e6589
Oliver Batchelor <saulzar@gmail.com>
2019-04-28 18:37:56 +1200

    Update coco testi

--- scripts/multiclass_coco.sh
+++ scripts/multiclass_coco.sh
@@ -2,14 +2,16 @@
 base="/home/oliver/export/"
 cmd="python -m main"
 
-common="--lr 0.01  --input  'coco --path /local/storage/coco' --no_load --train_epochs 60 --crop_boxes --image_size 512 --batch_size 16 --epoch_size 8192 --log_dir /local/storage/logs/multiclass_coco/" 
+common="--lr 0.001  --input  'coco --path /storage/storage/coco' --no_load --train_epochs 60 --crop_boxes --image_size 512 --batch_size 16 --epoch_size 8192 --log_dir /local/storage/logs/multiclass_coco/" 
 prefix="/local/storage/export"
 
-subset1="cow,sheep,cat,dog,zebra,giraffe,elephant,bear"
-subset2="hotdog,pizza,donut,cake,cup,fork,knife,spoon"
+subset1="cow,sheep,cat,dog"
+subset2="zebra,giraffe,elephant,bear"
+subset3="hotdog,pizza,donut,cake"
+subset3="cup,fork,knife,spoon"
 
 
-for subset in $subset1 $subset2; 
+for subset in $subset1 $subset2 $subset3 $subset4; 
 do 
   bash -c "$cmd  $common --run_name $subset --model \"fcn --features 128\" --subset $subset"
   for i in $(echo $subset | sed "s/,/ /g")
